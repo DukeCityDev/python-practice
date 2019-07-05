49877a9dd21bde8c8b993e4941757baff6b3b0c4
DukeCityDev <danieleaton747@gmail.com>
2019-07-05 16:11:54 -0600

    Added one easy coding challenge

---
+++ unlucky_numbers.py
@@ -0,0 +1,10 @@
+def unlucky_numbers():
+    for i in range(1, 21):
+        if i == 4 or i == 13:
+            print(f"{i} is unlucky")
+        elif (i%2 == 0):
+            print(f'{i} is even')
+        else:
+            print(f'{i} is odd')
+
+unlucky_numbers()
